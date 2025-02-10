6ce5f0fe4e99ed8d36066931ac9eab01a945ab47
sandeshpokharel <sandeshpokharel444@gmail.com>
2025-02-10 16:08:06 -0600

    feat(send-dynamic-html-email): implemented basic feature to send dyanmic html email

---
+++ src/main/java/software/bytepushers/email/BpEmailAppApplication.java
@@ -0,0 +1,12 @@
+package software.bytepushers.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpEmailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpEmailAppApplication.class, args);
+	}
+}
---
+++ src/main/java/software/bytepushers/email/controller/MailController.java
@@ -0,0 +1,48 @@
+package software.bytepushers.email.controller;
+
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import software.bytepushers.email.dto.MailRequest;
+import software.bytepushers.email.service.SendMailService;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("email")
+public class MailController {
+    @Autowired
+    private SendMailService sendMailService;
+
+    @PostMapping("/sendEmail")
+    public String sendEmail(@RequestBody MailRequest mailRequest){
+        try {
+            sendMailService.sendEmail(mailRequest.getTo(), mailRequest.getSubject(), mailRequest.getBody());
+            return "Email sent";
+        } catch (MessagingException e){
+            return "email not sent" + e.getMessage();
+        }
+    }
+
+    @PostMapping("htmlEmail")
+    private void Run() throws MessagingException {
+        sendMailService.sendHtlmEmail();
+        System.out.println("Sent successfully");
+    }
+
+    @PostMapping("/uploadExcel")
+    public String uploadExcel(@RequestParam("file") MultipartFile file) throws MessagingException {
+        try{
+            sendMailService.sendEmailFromExcel(file);
+            return "Email sent successfully";
+        }catch (Exception e){
+            return "email not sent" + e.getMessage();
+        }
+    }
+}
