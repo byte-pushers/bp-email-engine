75d15a684a64666eef132baee667a586c8334584
sandeshpokharel <sandeshpokharel444@gmail.com>
2025-02-28 14:51:41 -0600

    HTML email

--- src/main/java/software/bytepushers/email/controller/MailController.java
+++ src/main/java/software/bytepushers/email/controller/MailController.java
@@ -1,18 +1,25 @@
 package software.bytepushers.email.controller;
 
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import software.bytepushers.email.service.SendMailService;
 
+import java.util.Date;
+
 @RestController
 public class MailController {
     @Autowired
     private SendMailService sendMailService;
     @GetMapping("sendEmail")
-    public String sendEmail() {
-        sendMailService.sendMail("tonte.pouncil@gmail.com", "Test Email", "Hello! This is a demo email for our Bytepushers flagship email project.");
-        return "Sent successfully";
+
+    private void Run() throws MessagingException{
+        sendMailService.sendHtlmEmail();
     }
 }
