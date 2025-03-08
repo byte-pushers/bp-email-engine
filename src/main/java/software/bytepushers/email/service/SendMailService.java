04a88c58d0df0b909902ee477c4bf4707c3858ae
sandeshpokharel <sandeshpokharel444@gmail.com>
2025-03-07 18:59:02 -0600

    Initial commit

--- src/main/java/software/bytepushers/email/service/SendMailService.java
+++ src/main/java/software/bytepushers/email/service/SendMailService.java
@@ -6,6 +6,7 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,32 +15,28 @@ public class SendMailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-//    public void sendEmail(String toEmail, String subject, String body) {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setTo(toEmail);
-//        message.setSubject(subject);
-//        message.setText(body);
-//        javaMailSender.send(message);
-//    }
-
-    public SendMailService(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
+    public void sendEmail(String to, String subject, String body) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom("bytepushers20@gmail.com");
+        javaMailSender.send(mimeMessage);
     }
 
     public void sendHtlmEmail() throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-
-        message.setFrom(new InternetAddress("bytepushers20@gmail.com"));
-        message.setRecipients(MimeMessage.RecipientType.TO, "bytepushers20@gmail.com");
-        message.setSubject("HTML test email");
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        mimeMessage.setFrom(new InternetAddress("bytepushers20@gmail.com"));
+        mimeMessage.setRecipients(MimeMessage.RecipientType.TO, "bytepushers20@gmail.com");
+        mimeMessage.setSubject("HTML test email");
 
         String html =
                 "<html><body>" +
                         "<h1>Hello John </h1> from html email!!!" +
                         "</body>" +
                         "</html>";
-
-        message.setContent(html, "text/html; charset=utf-8");
-        javaMailSender.send(message);
+        mimeMessage.setContent(html, "text/html; charset=utf-8");
+        javaMailSender.send(mimeMessage);
     }
 }
