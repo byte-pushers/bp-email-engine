7d78804d15db48770310bceb068046a048e132d9
sandeshpokharel <sandeshpokharel444@gmail.com>
2025-03-07 18:59:02 -0600

    Initial commit

--- src/main/java/software/bytepushers/email/controller/MailController.java
+++ src/main/java/software/bytepushers/email/controller/MailController.java
@@ -7,24 +7,29 @@ import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import software.bytepushers.email.dto.MailRequest;
 import software.bytepushers.email.service.SendMailService;
 
 import java.util.Date;
 
 @RestController
+@RequestMapping("email")
 public class MailController {
     @Autowired
     private SendMailService sendMailService;
 
-//    @GetMapping("sendEmail")
-//    public void run(String... args) throws Exception {
-//        sendMailService.sendEmail("bytepushers20@gmail.com", "Email from angusMail", "Hello World!! This is an test email using angusMail");
-//        System.out.println("Email sent successfully!");
-//    }
+    @PostMapping("/sendEmail")
+    public String sendEmail(@RequestBody MailRequest mailRequest){
+        try {
+            sendMailService.sendEmail(mailRequest.getTo(), mailRequest.getSubject(), mailRequest.getBody());
+            return "Email sent";
+        } catch (MessagingException e){
+            return "email not sent" + e.getMessage();
+        }
+    }
 
-    @GetMapping("sendEmail")
+    @PostMapping("htmlEmail")
     private void Run() throws MessagingException {
         sendMailService.sendHtlmEmail();
         System.out.println("Sent successfully");
